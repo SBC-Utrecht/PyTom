52bacc27f5ecf4e09347c61fbef3b8e1f6a07d6d
Gijs van der Schot <gijsschot@gmail.com>
2020-01-07 12:06:40 +0100

    Script for generation of tight mask has been added.

---
+++ tutorials/tutorial_1/gen_mask.py
@@ -0,0 +1,72 @@
+from pytom.tompy.io import read, write
+import sys
+from pylab import *
+from skimage.morphology import *
+from scipy.ndimage.morphology import binary_fill_holes
+from scipy.ndimage import label
+from scipy.ndimage.filters import gaussian_filter
+from scipy.ndimage import median_filter
+
+if __name__=='__main__':
+
+    import sys
+    from pytom.tools.script_helper import ScriptHelper, ScriptOption
+    from pytom.tools.parse_script_options import parse_script_options
+    from pytom.basic.structures import ParticleList
+
+    options = [ScriptOption(['-f', '--fileName'], 'Filename of model.', True, False),
+               ScriptOption(['-o', '--outputName'], 'Filename of mask file.', True, False),
+               ScriptOption(['-h', '--help'], 'Help.', False, True)]
+
+    helper = ScriptHelper(sys.argv[0].split('/')[-1], # script name             
+                          description='Extract tilt images from mrcstack, and creation of meta data file.',
+                          authors='Gijs van der Schot',
+                          options=options)
+
+    if len(sys.argv) == 1:
+        print(helper)
+        sys.exit()
+
+    try:
+        filename, outdir, help = parse_script_options(sys.argv[1:], helper)
+    except Exception as e:
+        print(e)
+        sys.exit()
+
+    if help is True:
+        print(helper)
+        sys.exit()
+
+
+    if os.path.exists(filename):
+        data = read(filename)
+    else:
+        print(helper)
+        sys.exit()
+
+    
+    data = read(filename)
+    mask = zeros_like(data, dtype=int)
+    mask[data < data.mean()-data.std()] = 1
+    mask = remove_small_objects(mask.astype(bool))
+    mask = binary_fill_holes(mask)
+
+    l, n = label(mask)
+
+    dx,dy,dz = data.shape
+    mask = (l==l[dx//2,dy//2,dz//2])
+    mask = binary_dilation(mask)
+    mask = median_filter(mask, 6)
+    mask = binary_dilation(mask)
+    mask = binary_dilation(mask)
+
+    #for i in range(10):
+    #    mask = binary_dilation(mask)
+
+    mask = gaussian_filter(mask*100.,2)
+    mask[mask>78] = 78.
+    mask /= mask.max()
+
+
+    write(outname, mask.astype(float32))
+
