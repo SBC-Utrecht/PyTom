7813059e24dcfa614d5f819ba86bc1968e9dd90f
Gijs Schot <gijsschot@gmail.com>
2020-12-07 20:25:43 +0100

    made angles a input flag / script option.\n - issue #41 closed

--- bin/mrcs2mrc.py
+++ bin/mrcs2mrc.py
@@ -23,6 +23,7 @@ if __name__=='__main__':
     from pytom.tools.script_helper import ScriptHelper, ScriptOption
     from pytom.tools.parse_script_options import parse_script_options
     from pytom.basic.structures import ParticleList
+    from numpy import arange
 
     options = [ScriptOption(['-f', '--fileName'], 'Filename of mrc stack.', True, False),
                ScriptOption(['-t', '--targetDir'], 'Folder in which the output files are saved', True, False),
@@ -30,6 +31,10 @@ if __name__=='__main__':
                ScriptOption(['-o', '--origDir'], 'Directory from which images in the stack originate. '
                                                 'It will use the prefix "sorted" to select the output file names.',
                             True, True),
+               ScriptOption(['-i', '--angularIncrement'], 'Angular Increment between tilts (typically 2 or 3 degrees). Default 2 degrees.', True, True),
+               ScriptOption(['-s', '--startingAngle'], 'Lowest angle in tiltseries. Default -60 degrees.', True, True),
+               ScriptOption(['-e', '--endingAngle'], 'Highest angle in tiltseries. Default 60 degrees .', True, True),
+               ScriptOption(['--metaFile'], 'Pytom-specific meta file containing the tiltangles of the images.', True, True),
                ScriptOption(['-m', '--mdoc'], 'Create truncated mdoc files.', False, True),
                ScriptOption(['-h', '--help'], 'Help.', False, True)]
 
@@ -44,7 +49,7 @@ if __name__=='__main__':
         sys.exit()
 
     try:
-        filename, outdir, prefix, origdir, mdoc, help = parse_script_options(sys.argv[1:], helper)
+        filename, outdir, prefix, origdir, increment, startAngle, endAngle, metafile, mdoc, help = parse_script_options(sys.argv[1:], helper)
     except Exception as e:
         print(e)
 
@@ -70,12 +75,27 @@ if __name__=='__main__':
     if not prefix[-1] == '_': prefix += '_'
 
 
+    increment = 2 if increment is None else float(increment)
+    startAngle = -60 if startAngle is None else float(startAngle)
+    endAngle = 60 if endAngle is None else float(endAngle)
+    angles = arange(startAngle, endAngle + increment/2, increment)
+
+
+    if not metafile is None and os.path.exists(metafile):
+        from pytom.basic.datatypes import DATATYPE_METAFILE
+        from pytom.gui.guiFunctions import loadstar
+        metadata = loadstar(metafile, dtype=DATATYPE_METAFILE)
+        angles = metadata['TiltAngle']
+
     x,y,z = data.shape
     sliceNR = min(x,y,z)
 
-    angles = range(-60,61,2)
+    if len(angles) != sliceNR:
+        raise Exception('Angles and number of images do not coincide.')
+
     if mdoc:
-        mdocname = os.path.join(outdir, os.path.basename(filename).replace('.mrc', '.mdoc').replace('.ali','.mdoc'))
+        mdocname = os.path.join(outdir, os.path.basename(filename).replace('.mrc', '.mdoc').replace('.ali','.mdoc').replace('.st','.mdoc'))
+        print(mdocname)
         mdocfile = open(mdocname, 'w')
 
     d = '''[ZValue = {}]
@@ -105,7 +125,8 @@ SubFramePath = X:\{}
         #print(f'extracted {os.path.basename(outname)} into {outdir}')
         #write(outname, data[:,:,sliceId], tilt_angle=tiltangle)
         #mrcfile.new(outname, data[sliceId, :,:].astype('float32'), overwrite=True)
-        if mdoc: mdocfile.write(d.format(sliceId, angles[sliceId], os.path.basename(out_names[sliceId])))
+        if mdoc:
+            mdocfile.write(d.format(sliceId, angles[sliceId], os.path.basename(out_names[sliceId])))
 
         extract_single_image(*out[-1])
 
