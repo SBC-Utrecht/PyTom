eb44e3aaeaed21ef5a15155c3d60fea2b6cf985d
Gijs Schot <gijsschot@gmail.com>
2021-03-26 10:28:23 +0100

    minor changes to templateMatchingCandidateExtraction

--- bin/templateMatchingCandidateExtractionSingleGPU.py
+++ bin/templateMatchingCandidateExtractionSingleGPU.py
@@ -41,9 +41,9 @@ if __name__ == '__main__':
 
     helper = ScriptHelper2(
         sys.argv[0].split('/')[-1],  # script name
-        description='Template matching and peak extraction on GPU.',
+        description='Template matching and peak extraction on a single GPU.',
         authors='GvdS',
-        options=[ScriptOption2(['--templateMatching'], 'Template matching', 'no arguments', 'optional'),
+        options=[ScriptOption2(['--templateMatching'], 'This flag activates template matching', 'no arguments', 'optional'),
                  ScriptOption2(['-v', '--volume'], 'Filename of volume', 'file', 'optional'),
                  ScriptOption2(['-t', '--template'], 'Filename of template', 'file', 'optional'),
                  ScriptOption2(['-m', '--mask'], 'Filename of mask', 'file', 'optional'),
@@ -56,14 +56,14 @@ if __name__ == '__main__':
                  ScriptOption2(['--scoreVolume'], 'Filename of volume', 'file', 'optional', 'scores.mrc'),
                  ScriptOption2(['--rotationVolume'], 'Filename of volume', 'file', 'optional', 'angles.mrc'),
 
-                 ScriptOption2(['--extractPeaks'], 'Extract peaks', 'no arguments', 'optional'),
+                 ScriptOption2(['--extractPeaks'], 'This flag activates peaks extraction', 'no arguments', 'optional'),
                  ScriptOption2(['--particleList'], 'Filename of output particleList', 'string', 'optional', 'particleList.xml'),
                  ScriptOption2(['--particlePath'], 'Path of particles', 'string', 'optional', './'),
-                 ScriptOption2(['-r', '--radiusParticle'], 'Radius of the particle', 'float', 'optional', 8),
+                 ScriptOption2(['-r', '--radiusParticle'], 'Radius of the particle', 'int', 'optional', 8),
                  ScriptOption2(['-n', '--numberOfParticles'], 'Number of particles', 'int', 'optional', 1),
                  ScriptOption2(['--minScore'], 'Minimum correlation score', 'float', 'optional', 0.1),
-                 ScriptOption2(['--projectDir'], 'Project Directory (GUI)', 'directory', 'optional'),
-                 ScriptOption2(['--NoOutputVolumes'], 'write intermediate output files', 'no arguments', 'optional')])
+                 ScriptOption2(['--projectDir'], 'Add this Project Directory (GUI) to the xml', 'directory', 'optional'),
+                 ScriptOption2(['--NoOutputVolumes'], 'Do not write intermediate output files', 'no arguments', 'optional')])
 
     options = parse_script_options2(sys.argv[1:], helper)
 
@@ -74,20 +74,32 @@ if __name__ == '__main__':
 
     if templateMatch:
         rotations = GlobalSampling(angles)
-        volume = read(volumeFileName, keepnumpy=True)
+        volume = read(volumeFileName, keepnumpy=False)
         if start_end.sum():
             volume = volume[start_end[0]:start_end[3], start_end[1]:start_end[4], start_end[2]:start_end[5]]
             print(volume.shape)
 
-        template = read(template, keepnumpy=True)
-        mask = read(mask, keepnumpy=True)
+        template = read(template, keepnumpy=False)
+        mask = read(mask, keepnumpy=False)
 
         wedge = Wedge(wedge_angles)
 
-        [scoreVolume, angleVolume, a,b] = templateMatchingGPU(volume, template, rotations, scoreFunction, mask,
-                                                       wedgeInfo=wedge, gpuID=[gpu])
+        if 0:
+            from pytom.tompy.transform import resize
+            volume = resize(volume, 1/2)
+            template = resize(template, 1/2)
+            #mask = resize(mask, 1/2, 'Spline')
+
+        if 0:
+            from pytom.tompy.filter import bandpass
+            volume = bandpass(volume,0, 60, 3)
+            template = bandpass(template,0, 60, 3)
+
+        [scoreVolume, angleVolume, a,b] = templateMatchingGPU(volume.get(), template.get(), rotations, scoreFunction, mask.get(),
+                                                       wedgeInfo=wedge, gpuID=gpu)
 
         if not no_output:
+            print(scoreVolume.max())
             write(scores, scoreVolume)
             write(rots, angleVolume)
 
@@ -96,7 +108,7 @@ if __name__ == '__main__':
         binning, ref = getBinningFactorAndReferenceMarker(volumeFileName)
 
         res = extractCandidatesWithoutJobFile(volumeFileName, scores, rots, angles, radius, num_particles,
-                                        min_score, scoringFunction=FLCFScore, offset=start_end[::2])
+                                        min_score, scoringFunction=FLCFScore, offset=start_end[:3])
 
         if plFilename:
 
@@ -113,7 +125,7 @@ if __name__ == '__main__':
                 if not proj_dir is None:
                     newParticle.getInfoGUI().setProjectDir(proj_dir)
                 newParticle.getPickPosition().setBinningFactor(binning)
-                newParticle.getPickPosition().setRefMarkIndex(ref)
+                newParticle.getPickPosition().setRefMarkerIndex(ref)
                 pl.append(newParticle)
 
             pl.toXMLFile(plFilename)
