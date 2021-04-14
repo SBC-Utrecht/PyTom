b75a93e86bb8cb6ded7d0e73bc37c8c55f6963b6
Gijs Schot <gijsschot@gmail.com>
2021-04-14 14:55:19 +0200

    pytom can now be imported into python with reduced functionality

--- pytom/__init__.py
+++ pytom/__init__.py
@@ -1,13 +1,18 @@
 #list of modules that will be available when from pytom import * is called
 
 __all__ = ["alignment", "angles", "basic", "bin", "classification","cluster","frm","frontend","gui", "gpu", "image2D","localization","parallel","pytomc",
-           "plotting","reconstruction","score","simulation",'tompy', "tools", "unit_tests", "visualization", 'voltools']
+           "plotting", "reconstruction","score", "simulation", 'tompy', "tools", "visualization", 'voltools']
 __version__ = "0.994"
 
-import pytom_volume
-import pytom_numpy
 import pytom.basic.fourier as fourier
 import pytom.basic.files as files
 import pytom.tools.files as fileTools
 import pytom.basic.structures as structures
+import pytom.tompy.io as io
 
+try:
+    import pytom_volume
+    import pytom_numpy
+
+except:
+    print('The C-functionality of PyTom failed to load. For full functionality, please run pytom or ipytom.')
