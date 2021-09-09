b3593847a165aee7f28d4cdd434cc38c9878aa74
gijsschot <gijsschot@gmail.com>
2021-09-09 20:30:46 +0200

    interactive wrt condainstalldir

--- setup.py
+++ setup.py
@@ -2,43 +2,35 @@ from setuptools import setup, Extension, find_packages
 from setuptools.command.install import install
 
 import subprocess
-import setuptools.command.build_py
+#import setuptools.command.build_py
 
 import os
 from pytom import __version__
+
+
+print('Would you like to add a miniconda environment directory to your installation? Example: /home/user/miniconda3/envs/pytom_env')
+condadir = input('\nType Path: ')
+
+
 
 def find_executables():
     folder = 'pytom/bin'
     a =  [f'{folder}/{e}' for e in os.listdir(folder) if os.path.isfile(f'{folder}/{e}') and not '__' in e]
     return a
 
-class BuildPyCommand(setuptools.command.build_py.build_py):
-    """Custom build command."""
 
+class CustomInstall(install):
     def run(self):
         import sys
         commandPullSubmodules = 'git submodule update --init --recursive'
         process = subprocess.Popen(commandPullSubmodules, shell=True, cwd="./")
         process.wait()
-
+        #
         version = f'{sys.version_info[0]}.{sys.version_info[1]}'
         commandInstall = f'python{version} compile.py --target all'
+        if os.path.exists(condadir): commandInstall += f' --minicondaEnvDir {condadir}' 
         process = subprocess.Popen(commandInstall, shell=True, cwd="pytom/pytomc")
         process.wait()
-
-
-
-class CustomInstall(install):
-    def run(self):
-        # import sys
-        # commandPullSubmodules = 'git submodule update --init --recursive'
-        # process = subprocess.Popen(commandPullSubmodules, shell=True, cwd="./")
-        # process.wait()
-        #
-        # version = f'{sys.version_info[0]}.{sys.version_info[1]}'
-        # commandInstall = f'python{version} compile.py --target all'
-        # process = subprocess.Popen(commandInstall, shell=True, cwd="pytom/pytomc")
-        # process.wait()
 
         install.run(self)
 
@@ -62,7 +54,7 @@ setup(
     name='pytom',
     version=__version__,
     packages=find_packages(),
-    #package_dir={'pytom':'pytom'},
+    package_dir={'pytom':'pytom'},
     # package_data={'pytom':["alignment"]},
     author='`FridoF',
     author_email='gijsschot@gmail.com',
@@ -72,7 +64,7 @@ setup(
         'gpu': ['cupy'],
         'gui': ['PyQt5', 'pyqtgraph', 'mrcfile'],
         'all': ['cupy', 'PyQt5', 'pyqtgraph', 'mrcfile']},
-    cmdclass={'build_py': BuildPyCommand},
+    cmdclass={'install': CustomInstall},
     include_package_data=True,
     scripts=find_executables(),
     test_suite='nose.collector',
