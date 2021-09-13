a09f21d837fc9c1167fc5d9ed633095a88443dea
gijsschot <gijsschot@gmail.com>
2021-09-13 12:30:07 +0200

    added script for install mi miconda

---
+++ installMiniconda.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-py38_4.10.3-Linux-x86_64.sh
+chmod +x Miniconda3-py38_4.10.3-Linux-x86_64.sh
+./Miniconda3-py38_4.10.3-Linux-x86_64.sh
