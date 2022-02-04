57cf4bea513f55d66ac478b8a60ffa3afb5d8559
McHaillet <martenchaillet@gmail.com>
2022-02-04 12:12:24 +0100

    fixed printing of kkk upon every key press event in GUI

--- pytom/gui/guiStructures.py
+++ pytom/gui/guiStructures.py
@@ -2226,7 +2226,6 @@ class KeyPressGraphicsWindow(pg.GraphicsWindow):
 
     def __init__(self, *args, **kwargs):
         super(KeyPressGraphicsWindow,self).__init__(*args, **kwargs)
-        print('\n\nkkk')
 
     def keyPressEvent(self, ev):
         self.scene().keyPressEvent(ev)
