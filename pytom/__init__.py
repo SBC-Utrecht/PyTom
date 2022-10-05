ab23da208e3732f060ef4438c99136d9c7f774bd
McHaillet <martenchaillet@gmail.com>
2022-10-05 11:27:31 +0200

    fixed bug in __init__.py where modules were imported

--- pytom/__init__.py
+++ pytom/__init__.py
@@ -1,16 +1,9 @@
-#list of modules that will be available when from pytom import * is called
+# list of modules that will be available when from pytom import * is called
 
-
-__all__ = ["alignment", "angles", "basic", "bin", "classification","gui", "gpu","localization","parallel","pytomc",
+__all__ = ["alignment", "angles", "basic", "bin", "classification", "gui", "gpu", "localization", "parallel", "pytomc",
            "plotting", "reconstruction", "simulation", 'agnostic', "tools", 'voltools']
 
 __version__ = "0.995"
-
-import pytom.basic.fourier as fourier
-import pytom.basic.files as files
-import pytom.tools.files as fileTools
-import pytom.basic.structures as structures
-
 
 try:
     import pytom_volume
@@ -18,5 +11,5 @@ try:
     import pytom_mpi
     import pytom_freqweight
     import pytom_fftplan
-except:
+except (ModuleNotFoundError, ImportError):
     print('The C-functionality of PyTom failed to load. For full functionality, please run pytom or ipytom.')
